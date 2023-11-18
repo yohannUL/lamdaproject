6ae909513fd5f3cc2118874e91f74091c537dddc
yohannUL <yoeki@ulaval.ca>
2023-11-18 01:32:03 -0500

    grosserefonte +ajout de la fonctionafficher et retier la boucle de la fontion produire historique

--- phase1.py
+++ phase1.py
@@ -1,9 +1,17 @@
-"""Importation de librairie pour le module d'affichage des valeurs boursiers."""
-import json
-import datetime
-import argparse
-import requests
+"""
+Générer un interpréteur de commande.
 
+Returns:
+    Un objet Namespace tel que retourné par parser.parse_args().
+    Cet objet aura l'attribut «symboles» représentant la liste des
+    symboles à traiter, et les attributs «début», «fin» et «valeur»
+    associés aux arguments optionnels de la ligne de commande.
+"""
+import argparse#importation
+import json#importation
+from datetime import date, datetime#importation
+import datetime#importation
+import requests#importation
 
 
 
@@ -25,82 +33,99 @@ def analyser_commande():
     # vous pourriez aussi avoir à ajouter des arguments dans ArgumentParser(...)
     parser.add_argument(
         'symbole',
-        type=str,
-        nargs="+",
-        help="Nom d'un symbole boursier",
+        nargs='+',
+        # required="False",
+        help='Nom d\'un symbole boursier',
     )
     parser.add_argument(
         "-d", "--début",
-        metavar="DATE",
+        metavar="Date",
+        # action = 'store_true',
         help=" Date recherchée la plus ancienne (format: AAAA-MM-JJ)",
     )
     parser.add_argument(
         "-f", "--fin",
-        metavar="DATE",
+        metavar="Date",
+        # action = 'store_true',
         help="Date recherchée la plus récente (format: AAAA-MM-JJ)",
     )
     parser.add_argument(
         '-v', '--valeur',
+        metavar='{fermeture,ouverture,min,max,volume}',
         default='fermeture',
-        type=str,
-        choices=["fermeture", "ouverture", "min,max", "volume"],
+        # action = "store_true",
         help="La valeur désirée (par défaut: fermeture)",
     )
     return parser.parse_args()
 
 
-def produire_historique():
+recuperer = analyser_commande()
+symbole = recuperer.symbole
+debut = recuperer.début
+fin = recuperer.fin
+valeur = recuperer.valeur
+
+
+def produire_historique(symbole, debut, fin, valeur):
     """
-    affichage des valeurs boursiers.
+    Générer un interpréteur de commande.
 
     Returns:
-        notre module produit le titre de l'action la valeur la date de debut et la date de fin et
-         ensuite retourne un liste de la date puis de la valeur de cette action a la dte donner
-
-        exemple 1:
-            python phase1.py -d=2019-02-18 -f=2019-02-24 goog
-            titre=goog: valeur=fermeture, début=datetime.date(2019, 2, 18), fin=
-            datetime.date(2019, 2, 24)
-            [(datetime.date(2019, 2, 19), 1118.56), (datetime.date(2019, 2, 20), 1113.8),
-            (datetime.date(2019, 2, 21), 1096.97), (datetime.date(2019, 2, 22), 1110.37)]
-
-        exemple 2:
-            python phase1.py -v=volume -f=2019-02-22 goog
-            titre=goog: valeur=volume, début=datetime.date(2019, 2, 22),
-            fin=datetime.date(2019, 2, 22) [(datetime.date(2019, 2, 22),
-            1049545)]
+        Un objet Namespace tel que retourné par parser.parse_args().
+        Cet objet aura l'attribut «symboles» représentant la liste des
+        symboles à traiter, et les attributs «début», «fin» et «valeur»
+        associés aux arguments optionnels de la ligne de commande.
     """
-    list_date=[]
-    get_parameters=analyser_commande()
-    reponse_finale= ""
-    for elemnt_symbole in get_parameters.symbole:
-        url = f'https://pax.ulaval.ca/action/{elemnt_symbole}/historique/'
-        if get_parameters.début is None and get_parameters.fin is not None:
-            get_parameters.début=get_parameters.fin
-        # check is date de fin n'existe pas il prend today
-        if  get_parameters.début is not None and get_parameters.fin is None:
-            get_parameters.fin=str(datetime.date.today())
-        list_date.append(datetime.datetime.strptime(get_parameters.début,'%Y-%m-%d').date())
-        list_date.append(datetime.datetime.strptime(get_parameters.fin,'%Y-%m-%d').date())
+    liste = []
 
-        params = {
-            'début': get_parameters.début,
-            'fin': get_parameters.fin,
-        }
-        table_réponse = requests.get(url=url, params=params, timeout=60)
-        table_réponse = json.loads(table_réponse.text)
-        table=[]
-        for key in table_réponse['historique'].keys():
-            table.append((datetime.datetime.strptime(key, '%Y-%m-%d').date(),
-                          table_réponse['historique'][key][get_parameters.valeur]))
-        repons= ""
-        repons= f"titre={elemnt_symbole}: valeur={get_parameters.valeur}, début="
-        repons+= f"datetime.date({list_date[0].year}, {list_date[0].month}, {list_date[0].day}), "
-        repons+= f"fin=datetime.date({list_date[1].year}, {list_date[1].month}, {list_date[1].day})"
-        table.reverse()
-        repons= repons + "\n" + str(table)
-        reponse_finale= reponse_finale + "\n" + repons
-    return reponse_finale
+    url = f'https://pax.ulaval.ca/action/{str(symbole[0])}/historique/'
+    if debut is None and fin is not None:
+        debut = fin
+    if debut is not None and fin is None:
+        fin = str(datetime.date.today())
+    params = {
+        'début': debut,
+        'fin': fin,
+    }
+    table_json = requests.get(url=url, params=params)
+    table_json = json.loads(table_json.text)
+    historique = table_json["historique"]
 
-resultat=produire_historique()
-print(resultat)
+    for key in historique.keys():
+        liste.append((date.fromisoformat(key), historique[key][valeur]))
+    liste.reverse()
+    return liste
+
+
+#print(produire_historique(symbole, début, fin, valeur))
+
+
+# print(symbole)
+
+def afficher():
+    """
+Générer un interpréteur de commande.
+
+Returns:
+    Un objet Namespace tel que retourné par parser.parse_args().
+    Cet objet aura l'attribut «symboles» représentant la liste des
+    symboles à traiter, et les attributs «début», «fin» et «valeur»
+    associés aux arguments optionnels de la ligne de commande.
+"""
+    list_date = []
+    for element_symbole in symbole:
+        list_date.append(datetime.datetime.strptime(debut, '%Y-%m-%d').date())
+        list_date.append(datetime.datetime.strptime(fin, '%Y-%m-%d').date())
+
+        repon = ""
+        repon = f"titre={element_symbole}: valeur={valeur}, début="
+        repon += f"datetime.date({list_date[0].year}, {list_date[0].month}, {list_date[0].day}), "
+        repon += f"fin=datetime.date({list_date[1].year}, {list_date[1].month}, {list_date[1].day})"
+
+        histoire = produire_historique(list(element_symbole), debut, fin, valeur)
+        #print(histoire)
+        repon += "\n" + str(histoire)
+        print(repon)
+
+afficher()
+#print(symbole)
